1c7397fffde5b297effc79dcf6c04c86ed82fb0c
Basavaraj <bjaganuri@yahoo.in>
2017-08-16 18:11:08 +0530

    cors enabled

--- app.js
+++ app.js
@@ -61,6 +61,11 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname , 'public')));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
 
 app.use(session({
 	store: store,
