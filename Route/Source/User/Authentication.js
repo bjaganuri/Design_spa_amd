c8430d993c1fcee397bfb138d522c779f4d18513
bjaganuri <bjaganuri@gmail.com>
2017-10-23 13:58:45 +0530

    Update Authentication.js

--- Route/Source/User/Authentication.js
+++ Route/Source/User/Authentication.js
@@ -16,11 +16,11 @@ passport.use(new LocalStrategy(function(username, password, done) {
 			return done(null,false,{message:"Unknown User"});
 		}
 
-		if(user.opState === "LOCKED" && !user.isLocked && user.lockedBy !== "Unknown Source") {
+		if(user.opState === "LOCKED" && !user.isLocked && user.lockedBy !== "System") {
 			return done(null,false,{message:"Your account has been locked permanently by "+ user.lockedBy +" with " + user.lockComments + " as comments pls contact admin to unlock your account"});
 		}
-		else if(user.opState === "LOCKED" && !user.isLocked && user.lockedBy === "Unknown Source") {
-			return done(null,false,{message: "Your account has been locked because " + user.lockedBy + " has tried to login many times, to unlock pls contact admin"});
+		else if(user.opState === "LOCKED" && !user.isLocked && user.lockedBy === "System") {
+			return done(null,false,{message: "Your account has been locked permanently by " + user.lockedBy + " because you have exceeded the maximum number of login attemapts, to unlock pls contact admin"});
 		}
 
 		if (user.opState === "LOCKED" && user.isLocked) {
